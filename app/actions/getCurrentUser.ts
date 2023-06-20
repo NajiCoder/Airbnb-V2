728e21ff37f45411d9c4dd364581899785242f07
Naji <lemiine987@gmail.com>
2023-06-20 14:02:20 +0300

    Login fuctionality added

---
+++ app/actions/getCurrentUser.ts
@@ -0,0 +1,35 @@
+import { getServerSession } from "next-auth/next";
+
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import prisma from "@/app/libs/prismadb";
+
+export async function getSession() {
+  return await getServerSession(authOptions);
+}
+
+export async function getCurrentUser() {
+  try {
+    const session = await getSession();
+
+    if (!session?.user?.email) {
+      console.log("no session user email");
+      return null;
+    }
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email as string,
+      },
+    });
+
+    if (!currentUser) {
+      console.log("no current user");
+      return null;
+    }
+
+    return currentUser;
+  } catch (error: any) {
+    console.log(error);
+    return null;
+  }
+}
